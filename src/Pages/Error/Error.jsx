aceee7acb0041c61eb7ce2c0a6f32984a350b949
AK Rasel <raselak101010@gmail.com>
2023-10-21 13:52:23 +0600

    Error page add and ui compelet

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div className="h-screen grid justify-center items-center">
-            <div className="text-center">
-                <div className="-mb-20">
-                <h3 className="  text-7xl font-bold">Uh-Oh...</h3>
-                <p className=" font-semibold text-lg">Pages Not Found</p>
+        <div className="h-screen grid justify-center items-center font-Montserrat">
+            <div className="text-center space-y-6">
+                
+                <div className="">
+                <h1 className="-mb-14 text-[250px] font-extrabold">4<span className="text-[#F0C543]">0</span>4</h1>
+                <p className=" font-semibold text-2xl">Pages Not Found</p>
+                <p className="text-lg font-medium text-gray-500">THe page you're looking for dose not seen to exist</p>
                 </div>
-                <h1 className="text-[250px] font-extrabold">4<span className="text-[#F0C543]">0</span>4</h1>
+                
+                
                 <Link to={'/'} className="btn  bg-[#F0C543] text-[#23393D] hover:bg-[#23393D]
                 hover:text-white
                 ">Goto Home</Link>
